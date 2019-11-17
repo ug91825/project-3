00da06b98da235014f8d744d5425c61a02e64307
Joe Z <joe@joezeng.com>
2019-11-17 01:49:52 -0500

    Added Twitch links.

--- res/ga_links.js
+++ res/ga_links.js
@@ -4,3 +4,10 @@ function kofi_link() {
         'event_label': "Ko-fi",
     });
 }
+
+function twitch_link() {
+    gtag('event', "view_promotion", {
+        'event_category': "engagement",
+        'event_label': "Twitch",
+    });
+}
